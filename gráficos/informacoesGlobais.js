67e0f20d7e37ad881c8ff8d95cbf9d3c783fac19
Limas <125028001+FlaviaLimas@users.noreply.github.com>
2024-11-06 07:49:17 -0300

    06/11

--- gráficos/informacoesGlobais.js
+++ gráficos/informacoesGlobais.js
@@ -3,12 +3,16 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function vizuaçizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados)
+   const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+   const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+   const horas = parseInt(dados.tempo_medio)
+   const minutos = Math.round((dados.tempo_medio - horas) * 100)
+
+
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> de pessoas estão conectadas em alguma rede social  e passam em média <span>${dados.tempo_medio}</span> horas conectadas`
-    console.log(paragrafo)
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> de pessoas estão conectadas em alguma rede social  e passam em média <span>${horas} horas </span> e <span> ${minutos} minutos </span> conectadas.<br>E no Brasil são aproximadamente <span>${144} milhões</span> de pessoas conectadas e passam aproximadamente <span>${3} horas </span> e <span> ${31} minutos </span> conectadas`
+    
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
